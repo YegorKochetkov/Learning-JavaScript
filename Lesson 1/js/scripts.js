f117ebb7e5aae4e2042941f1c08b999d4333c3f2
Yegor Kochetkov <kegor.fx@gmail.com>
2021-10-28 12:22:43 +0300

    Lesson-1 Simple calculator

--- Lesson 1/js/scripts.js
+++ Lesson 1/js/scripts.js
@@ -1,6 +1,14 @@
 window.onload = function(e) {
 	var products = document.querySelectorAll('.items .item');
 	var totalSum = document.querySelector('.total-sum');
+
+	var addBtn = document.querySelector('input[name=add]');
+	var subtractBtn = document.querySelector('input[name=subtract]');
+	var multiplyBtn = document.querySelector('input[name=multiply]');
+	var divideBtn = document.querySelector('input[name=divide]');
+	var num1 = document.querySelector('input[name=num1]');
+	var num2 = document.querySelector('input[name=num2]');
+	var result = document.querySelector('.res');
 	
 	for(var i = 0; i < products.length; i++) {
 		products[i].onclick = function() {
@@ -18,5 +26,43 @@ window.onload = function(e) {
 			}
 		}
 		totalSum.innerHTML = 'Cart: ' + price;
+	}
+
+	addBtn.onclick = function() {
+		var res = parseInt(num1.value) + parseInt(num2.value);
+		result.innerHTML = res;
+		addBtn.disabled = true;
+	}
+
+	subtractBtn.onclick = function() {
+		var res = parseInt(num1.value) - parseInt(num2.value);
+		result.innerHTML = res;
+		subtractBtn.disabled = true;
+	}
+
+	multiplyBtn.onclick = function() {
+		var res = parseInt(num1.value) * parseInt(num2.value);
+		result.innerHTML = res;
+		multiplyBtn.disabled = true;
+	}
+	
+	divideBtn.onclick = function() {
+		var res = parseInt(num1.value) / parseInt(num2.value);
+		result.innerHTML = res;
+		divideBtn.disabled = true;
+	}
+
+	num1.onclick = function() {
+		addBtn.disabled = false;
+		subtractBtn.disabled = false;
+		multiplyBtn.disabled = false;
+		divideBtn.disabled = false;
+	}
+
+	num2.onclick = function() {
+		addBtn.disabled = false;
+		subtractBtn.disabled = false;
+		multiplyBtn.disabled = false;
+		divideBtn.disabled = false;
 	}
 }
