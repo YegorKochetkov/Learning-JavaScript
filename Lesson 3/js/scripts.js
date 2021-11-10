86b95a62ff0480b62de3246b29763b0b27d14845
Yegor Kochetkov <kegor.fx@gmail.com>
2021-11-10 16:17:10 +0200

    add callback and own library lesson

--- Lesson 3/js/scripts.js
+++ Lesson 3/js/scripts.js
@@ -18,8 +18,8 @@ function Popup(options) {
 	this.overlay.onclick = popup.close;
 }
 
-window.onload = function() {
-
+window.onload = function(e) {
+//modal window logic
 	var p = new Popup({
 		modal: '.modal',
 		overlay: '.overlay'
@@ -33,5 +33,43 @@ window.onload = function() {
 		var form = document.querySelector('.for-write-popup');
 		p.open(form.innerHTML);
 	};
-	
+
+//callback and own library lesson	
+    var inputs = document.querySelectorAll('.form2 .check');
+    
+    document.querySelector('.form2').onsubmit = function(e){
+        var error = false;
+        
+        for(var i = 0; i < inputs.length; i++){
+            if(inputs[i].value === ''){
+                inputs[i].classList.add('err');
+                error = true;
+            }
+        }
+        
+        if(error){
+            e.preventDefault();
+        }
+    }
+
+	let jqInputs = $('.check');
+
+	jqInputs.on('click', function() {
+		this.classList.remove('err');
+	});
+
+	jqInputs.on('focus', function() {
+		this.classList.add('focus');
+	});
+
+	jqInputs.on('blur', function() {
+		this.classList.remove('focus');
+	});
+
+	$('.items .item').on('click', function() {
+		$(this).fade(1000, function() {
+			this.style.opacity = 1;
+			this.style.display = 'block';
+		});
+	});
 }
