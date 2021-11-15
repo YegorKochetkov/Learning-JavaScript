7cbe48f21bc70c5e93a365a707b8d03e2911e142
Yegor Kochetkov <kegor.fx@gmail.com>
2021-11-15 13:46:49 +0200

    add slider task to lesson 4

--- Lesson 4/js/scripts.js
+++ Lesson 4/js/scripts.js
@@ -41,11 +41,58 @@ $(function() {
 	$('.faq .question').on('click', function() {
 		let question = $(this);
 		let answer = $(this).next();
-		
+
 		question.css('cursor','default');
 		$('.faq .question').not(question).css('cursor','pointer');
 		$('.faq .answer:visible').not(answer).slideUp(400);
 		// answer.slideToggle(400);
 		answer.slideDown(400);
 	})
+
+	//Slider from lesson 2 with jQuery
+		var slider = new Slider({
+			images: '.gallery-1 .photos img',
+			btnPrev: '.gallery-1 .buttons .prev',
+			btnNext: '.gallery-1 .buttons .next',
+			auto: false,
+		});
+			
+		function Slider(slider){
+			this.images = $(slider.images);
+			this.btnPrev = slider.btnPrev;
+			this.btnNext = slider.btnNext;
+			this.rate = slider.rate || 1500;
+			var i = 0;
+			var that = this;
+			
+			this.prev = function(){
+				that.images.eq(i).removeClass('showed');
+				i--;
+	
+				if(i < 0){
+					i = that.images.length - 1;
+				}
+	
+				that.images.eq(i).addClass('showed');
+			}
+	
+			this.next = function(){
+				that.images.eq(i).removeClass('showed');
+				i++;
+	
+				if(i >= that.images.length){
+					i = 0;
+				}
+	
+				that.images.eq(i).addClass('showed');
+			}
+			
+			if(slider.auto){
+				setInterval(that.next, that.rate);
+			}
+			else {
+				$(this.btnPrev).on('click', function() {that.prev();});
+				$(this.btnNext).on('click', function() {that.next();});
+			}
+		}	
 });
