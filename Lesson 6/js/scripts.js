08dbf0f9734f6338ce5d57ac0a7ab617cf0bd97b
Yegor Kochetkov <kegor.fx@gmail.com>
2021-12-06 16:15:58 +0200

    preparing files for the lesson 6

---
+++ Lesson 6/js/scripts.js
@@ -0,0 +1,49 @@
+$(function() {
+	//Own plugin for jQuery that duplicates fields
+	$('.duplicate input').on('click', function() {
+		$('.form2').duplicate({delimiter: ' + ', cnt: 2});
+	});
+
+	//Owl Carousel
+	let $items = $('.carousel .item');
+
+	$('.owl-carousel').owlCarousel({
+		loop:false,
+		margin:10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:3,
+				nav:true
+			},
+			1000:{
+				items:5,
+				nav:true
+			}
+		},
+		onInitialized: function(){
+			autoHeight($items);
+		},
+		onResized: function(){
+			autoHeight($items);
+		},
+		navText:["&nbsp;prev&nbsp;","&nbsp;next&nbsp;"]
+	});
+
+	function autoHeight($items) {
+		let max = 0;
+
+		$items.each(function() {
+			let h = $(this).find('> div').height();
+
+			if(h > max) {
+				max = h;
+			}
+		});
+		$items.css('min-height', max + 'px');
+	}
+});
